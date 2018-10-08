d43707bffff1e1ffb71ec139ab8f47b1f7f30f9c
Konrad Ryczek <konradryczek92@gmail.com>
2018-10-08 14:24:33 +0200

    Page update

--- js/script.js
+++ js/script.js
@@ -1,30 +1,92 @@
-$(function(){
-    $('.wrapper').click(function() {
-        var src = $(this).attr('style');
-        
-        var self = this;
-        console.log(src);
-        if ($('#lightbox').length > 0) {
-            $('#content').css('background-image', src);
-            $
-            $('#lightbox').fadeIn(400);
-        } else {
-            var lightbox = 
-            '<div id="lightbox" style="display:none">' +
-            '<div id="content">' +
-            '<h2>' + title + '<h2>'
-            '</div>' +  
-            '</div>';
-            
-            $('body').append(lightbox);
-            $('#content').css('background-image',src);
-            $('#lightbox').fadeIn(400);
-        }
-        
-            $('#lightbox').click(function() {
-            $('#lightbox').hide();
-        });
-    });
-});
+let modalId = $('#image-gallery');
 
- 
+$(document)
+  .ready(function () {
+
+    loadGallery(true, 'a.thumbnail');
+
+    //This function disables buttons when needed
+    function disableButtons(counter_max, counter_current) {
+      $('#show-previous-image, #show-next-image')
+        .show();
+      if (counter_max === counter_current) {
+        $('#show-next-image')
+          .hide();
+      } else if (counter_current === 1) {
+        $('#show-previous-image')
+          .hide();
+      }
+    }
+
+    /**
+     *
+     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+     * @param setClickAttr  Sets the attribute for the click handler.
+     */
+
+    function loadGallery(setIDs, setClickAttr) {
+      let current_image,
+        selector,
+        counter = 0;
+
+      $('#show-next-image, #show-previous-image')
+        .click(function () {
+          if ($(this)
+            .attr('id') === 'show-previous-image') {
+            current_image--;
+          } else {
+            current_image++;
+          }
+
+          selector = $('[data-image-id="' + current_image + '"]');
+          updateGallery(selector);
+        });
+
+      function updateGallery(selector) {
+        let $sel = selector;
+        current_image = $sel.data('image-id');
+        $('#image-gallery-title')
+          .text($sel.data('title'));
+        $('#image-gallery-image')
+          .attr('src', $sel.data('image'));
+        disableButtons(counter, $sel.data('image-id'));
+      }
+
+      if (setIDs == true) {
+        $('[data-image-id]')
+          .each(function () {
+            counter++;
+            $(this)
+              .attr('data-image-id', counter);
+          });
+      }
+      $(setClickAttr)
+        .on('click', function () {
+          updateGallery($(this));
+        });
+    }
+  });
+
+// build key actions
+$(document)
+  .keydown(function (e) {
+    switch (e.which) {
+      case 37: // left
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+          $('#show-previous-image')
+            .click();
+        }
+        break;
+
+      case 39: // right
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+          $('#show-next-image')
+            .click();
+        }
+        break;
+
+      default:
+        return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
